a8334eef6392a7c6e6ee1f770ecc2bf34fede5bb
Marek Vrbka <marekvrbka@gmail.com>
2019-08-24 22:10:58 +0200

    Upgrade functionality finished

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,7 +3,7 @@ extends Node
 
 export var cost = []
 export var tile_resource = []
-export var money = 3.0
+export var money := 3.0
 
 
 var upgrades = []
@@ -64,6 +64,10 @@ func save_serialization(save_game):
 	for i in save_nodes:
 		var node_data = i.call("save")
 		save_game.store_line(to_json(node_data))
+	save_nodes = get_tree().get_nodes_in_group("persist_update")
+	for i in save_nodes:
+		var node_data = i.call("save")
+		save_game.store_line(to_json(node_data))
 
 
 func save_game():
@@ -81,17 +85,21 @@ func load_globals(current_line):
 	money = current_line["money"]
 
 
-func load_game():
-	var save_game = File.new()
-	if not save_game.file_exists("user://savegame.save"):
-		return false
+func pre_load():
 	var save_nodes = get_tree().get_nodes_in_group("persist_1")
 	for i in save_nodes:
 		i.name = "queued"
 		i.queue_free()
+
+
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists("user://savegame.save"):
+		return false
+	pre_load()
 	var entities_deleted = false
 	save_game.open_compressed("user://savegame.save", File.READ, File.COMPRESSION_ZSTD)
-
+	# Don't touch this unless you absolutely have to.
 	while not save_game.eof_reached():
 		var line = save_game.get_line()
 		if validate_json(line):
@@ -99,6 +107,9 @@ func load_game():
 		var current_line = parse_json(line)
 		if current_line.has("money"):
 			load_globals(current_line)
+			continue
+		elif current_line.has("path"):
+			get_node(current_line["path"]).load_node(current_line)
 			continue
 		if !entities_deleted and current_line["filename"] == "res://scenes/Entity.tscn":
 			for entity in get_tree().get_nodes_in_group("persist_3"):
@@ -112,15 +123,19 @@ func load_game():
 		if current_line.has("tint_progress_r"):
 			new_object.tint_progress = Color(current_line["tint_progress_r"], current_line["tint_progress_g"], current_line["tint_progress_b"], current_line["tint_progress_a"])
 		for i in current_line.keys():
-#			if i == "filename" or i == "parent" or i == "tint_progress_r" or i == "tint_progress_g" or i == "tint_progress_b" or i == "tint_progress_a" or i == "pos_x" or i == "pos_y":
 			if i in ["filename", "parent", "tint_progress_r", "tint_progress_g", "tint_progress_b", "tint_progress_a", "pos_x", "pos_y"]:
 				continue
 			new_object.set(i, current_line[i])
+			
 	save_game.close()
+	after_load()
+	return true
+
+
+func after_load():
 	for node in get_tree().get_nodes_in_group("persist_1"):
 		node.after_load()
 	for node in get_tree().get_nodes_in_group("persist_2"):
 		node.after_load()
 	for node in get_tree().get_nodes_in_group("persist_3"):
 		node.after_load()
-	return true
--- scripts/UpgradeButton.gd
+++ scripts/UpgradeButton.gd
@@ -4,6 +4,7 @@ extends TextureRect
 export var cost = 100.0
 export var upgrade_id = -1
 export var text_label = "First line text"
+export var maxed_out_label = "maxed"
 export (Upgrades.UpgradeType) var type
 export var upgrade_multiplier = 4.0 / 3.0
 export var cost_multiplier = 1.5
@@ -14,35 +15,65 @@ export var increment = 3
 export var increment_limit = 10
 
 
+var level = 1
+
+
+var maxed_out = false
+
+
 
 func save():
 	var save_dict = {
 		"path" : get_path(),
 		"cost" : cost,
-		"hidden" : hidden,
-		"increment" : increment
+		"visible" : visible,
+		"increment" : increment,
+		"maxed_out" : maxed_out,
+		"level" : level
 		}
 	return save_dict
+
+
+func maxed():
+	var max_instance = preload("res://scenes/MaxedSprite.tscn").instance()
+	add_child(max_instance)
 
 
 func load_node(save_dict):
 	for i in save_dict:
 		if i != "path":
 			self.set(i, save_dict[i])
+	if maxed_out:
+		maxed()
+	$Label.text = str(level)
 
 
 func upgrade():
+	if Global.money < cost:
+		return
+	Global.money -= cost
 	if type == Upgrades.UpgradeType.UNLOCK:
 		get_tree().call_group(group_upgrade + str(increment), upgrade_function, unlock_type_argument[0])
 	else:
 		get_tree().call_group(group_upgrade, upgrade_function, upgrade_multiplier)
 		Upgrades.apply_upgrade(type, upgrade_id, upgrade_multiplier)
+	increment += 1
+	cost *= cost_multiplier
+	if increment > increment_limit:
+		maxed_out = true
+		maxed()
+		return
+	level += 1
+	$Label.text = str(level)
 
 
 func _on_UpgradeButton_gui_input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed:
+		if event.button_index == BUTTON_LEFT and event.pressed and !maxed_out:
 			upgrade()
 	if event is InputEventMouseMotion:
 		get_tree().call_group("mouse_in", "remove_mouse_in")
-		Global.set_text_hover_label(text_label + "\nCost: " + Global.get_magnified_string(cost))
+		if maxed_out:
+			Global.set_text_hover_label(maxed_out_label)
+		else:
+			Global.set_text_hover_label(text_label + "\nCost: " + Global.get_magnified_string(cost))
